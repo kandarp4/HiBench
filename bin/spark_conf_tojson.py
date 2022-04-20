079a5ffdcfc28c6ed68f4ae0903c5bbfa510bcbe
kandarp4 <kandarp411@gmail.com>
2022-04-20 12:41:13 +0530

    run_all update for sample test report

---
+++ bin/spark_conf_tojson.py
@@ -0,0 +1,34 @@
+
+import json
+import sys
+import re
+import os
+
+def log(*s):
+    if len(s) == 1:
+        s = s[0]
+    else:
+        s = " ".join([str(x) for x in s])
+    sys.stderr.write(str(s) + '\n')
+
+
+def main(filename):
+
+    spark_conf = dict()
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line:
+                continue  # skip empty lines
+            if line[0] == '#':
+                continue  # skip comments
+            try:
+                key, value = re.split("\s", line, 1)
+            except ValueError:
+                key = line.strip()
+                value = ""
+            spark_conf[key] = value
+    print(json.loads(spark_conf))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
