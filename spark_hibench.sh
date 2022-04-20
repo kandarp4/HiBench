7f43db06cdb0de25cc2cb720611b0f13dc94c8b2
kandarp4 <kandarp411@gmail.com>
2022-04-20 09:39:35 +0530

    spark auto conf added

--- spark_hibench.sh
+++ spark_hibench.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 sudo yum install git
-git clone https://github.com/Intel-bigdata/HiBench.git
 wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 tar xvf apache-maven-3.8.5-bin.tar.gz
 sudo mv apache-maven-3.8.5  /usr/local/apache-maven
 export M2_HOME=/usr/local/apache-maven
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
+cd HiBench
 mvn -Dspark=3.0 -Dscala=2.12 clean package
 mvn -Phadoopbench -Psparkbench -Dspark=3.0 -Dscala=2.12 clean package
