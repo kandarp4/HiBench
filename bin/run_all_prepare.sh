57303cdc79f6367cb94128393a9cc14ef97ac4a5
kandarp4 <kandarp411@gmail.com>
2022-04-20 15:47:09 +0530

    run_all update for sample test report

--- bin/run_all_prepare.sh
+++ bin/run_all_prepare.sh
@@ -6,48 +6,36 @@ root_dir=`cd "${current_dir}/.."; pwd`
 
 . ${root_dir}/bin/functions/color.sh
 
-readarray -t my_array < <(jq -c '.[]' conf/spark-sampled-conf.json)
 
-# iterate through the Bash array
-for item in "${my_array[@]}"; do
-#  original_name=$(jq '.original_name' <<< "$item")
-#  changed_name=$(jq '.changed_name' <<< "$item")
+for benchmark in `cat $root_dir/conf/benchmarks.lst`; do
+    if [[ $benchmark == \#* ]]; then
+        continue
+    fi
 
-    # Utilize your variables
-    python bin/write_spark_conf.py $item
+    echo -e "${UYellow}${BYellow}Prepare ${Yellow}${UYellow}${benchmark} ${BYellow}...${Color_Off}"
+    benchmark="${benchmark/.//}"
 
+    WORKLOAD=$root_dir/bin/workloads/${benchmark}
+    echo -e "${BCyan}Exec script: ${Cyan}${WORKLOAD}/prepare/prepare.sh${Color_Off}"
+    "${WORKLOAD}/prepare/prepare.sh"
 
+    result=$?
+    if [ $result -ne 0 ]
+    then
+    echo "ERROR: ${benchmark} prepare failed!"
+        exit $result
+    fi
 
-    for benchmark in `cat $root_dir/conf/benchmarks.lst`; do
-        if [[ $benchmark == \#* ]]; then
-            continue
-        fi
-
-        echo -e "${UYellow}${BYellow}Prepare ${Yellow}${UYellow}${benchmark} ${BYellow}...${Color_Off}"
-        benchmark="${benchmark/.//}"
-
-        WORKLOAD=$root_dir/bin/workloads/${benchmark}
-        echo -e "${BCyan}Exec script: ${Cyan}${WORKLOAD}/prepare/prepare.sh${Color_Off}"
-        "${WORKLOAD}/prepare/prepare.sh"
-
-        result=$?
-        if [ $result -ne 0 ]
-        then
-        echo "ERROR: ${benchmark} prepare failed!"
-            exit $result
-        fi
-        
 #        echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/spark${Color_Off}"
 #        echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/spark/run.sh${Color_Off}"
 #        $WORKLOAD/spark/run.sh
 
-        result=$?
-        if [ $result -ne 0 ]
-        then
-            echo -e "${On_IRed}ERROR: ${benchmark}/spark failed to run successfully.${Color_Off}"
-                $result
-        fi
-    done
-
-    echo "Run all done!"
+    result=$?
+    if [ $result -ne 0 ]
+    then
+        echo -e "${On_IRed}ERROR: ${benchmark}/spark failed to run successfully.${Color_Off}"
+            $result
+    fi
 done
+
+echo "Run all done!"
