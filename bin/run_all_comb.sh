1ffbb54eb185f12d08063b602d22f371bf9bcfb6
kandarp4 <kandarp411@gmail.com>
2022-04-20 14:30:53 +0530

    run_all update for sample test report

--- bin/run_all_comb.sh
+++ bin/run_all_comb.sh
@@ -36,75 +36,17 @@ for item in "${my_array[@]}"; do
         echo "ERROR: ${benchmark} prepare failed!"
             exit $result
         fi
-
-        for framework in `cat $root_dir/conf/frameworks.lst`; do
-        if [[ $framework == \#* ]]; then
-            continue
-        fi
-
-        if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
-            continue
-        fi
-        if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
-            continue
-        fi
-        if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "graph/pagerank" ] && [ $framework == "hadoop" ]; then
-          continue
-        fi
-        if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/pca" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/gbt" ] && [ $framework == "hadoop" ]; then
-             continue
-        fi
-        if [ $benchmark == "ml/rf" ] && [ $framework == "hadoop" ]; then
-              continue
-        fi
-        if [ $benchmark == "ml/svd" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/linear" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/lda" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/correlation" ] && [ $framework == "hadoop" ]; then
-            continue
-        fi
-        if [ $benchmark == "ml/summarizer" ] && [ $framework == "hadoop" ]; then
-             continue
-        fi
-
-        echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
+        
+        echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/spark${Color_Off}"
         echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/spark/run.sh${Color_Off}"
-        $WORKLOAD/${framework}/run.sh
+        $WORKLOAD/spark/run.sh
 
         result=$?
         if [ $result -ne 0 ]
         then
-            echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
+            echo -e "${On_IRed}ERROR: ${benchmark}/spark failed to run successfully.${Color_Off}"
                 $result
         fi
-        done
     done
 
     echo "Run all done!"
