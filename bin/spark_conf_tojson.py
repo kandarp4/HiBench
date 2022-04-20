1467ecf7affe547d347dd12e438997108c72235e
kandarp4 <kandarp411@gmail.com>
2022-04-20 13:42:51 +0530

    run_all update for sample test report

--- bin/spark_conf_tojson.py
+++ bin/spark_conf_tojson.py
@@ -29,7 +29,7 @@ def main(filename):
                 value = ""
             spark_conf[key] = value
 
-    print(str(spark_conf))
+    print(str(spark_conf).replace(" ", ""))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
